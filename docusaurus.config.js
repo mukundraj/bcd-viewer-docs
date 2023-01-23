dd008c71c20e9c34e3c7619e27406f13c540d534
Mukund Raj <mraj@broadinstitute.org>
2023-01-23 15:00:56 -0500

    Update downloads page title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'BrainCellData docs',
+  title: 'BrainCellData Docs',
   tagline: 'welcome',
   url: 'https://docs.braincelldata.org/',
   baseUrl: '/',
