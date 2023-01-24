09aee1bccd93c06cc1ccc526b9b4c1ce272f4680
Mukund Raj <mraj@broadinstitute.org>
2023-01-24 14:58:04 -0500

    Update downloads page

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -13,28 +13,74 @@ function HomepageHeader() {
     <div className="container">
       <ul>
         <h1>Data</h1>
-        <p>Download the data shown in the Brain Cell Data Viewer here for your own analysis.</p>
+
+        <p>Download the data shown in the Brain Cell Data Viewer here for your
+          own analysis. Citation and contact information for the following data
+          sets is available in the <a href="/about">About</a> page.</p>
+
       </ul>
 
       <ul>
         <li>
           <h3>Nissl Images</h3>
-          <p>Brief description and link to Nissl images.</p>
+          <p>Nissl images in TIFF format. Format: TIFF files in zipped folder. Size: ?? GB.
+          <br/>
+          <a href="">Download</a>
+          </p>
         </li>
 
         <li>
-          <h3>Bead Positions</h3>
-          <p>Brief description and link to bead positions.</p>
+          <h3>Slide-seq Bead Positions</h3>
+          <p>Slide-seq bead positions in the space of nissl images. Format: CSV files in zipped folder. Size: ?? GB.
+          <br/>
+          <a href="">Download</a>
+          </p>
         </li>
 
         <li>
           <h3>Gene Expression</h3>
-          <p>Brief description and link to gene expression data.</p>
+          <p>Number of UMI for each gene for all Slide-seq beads. Format: nBeads x nGenes matrix in Anndata format. Size: ?? GB
+          <br/>
+          <a href="">Download</a>
+          </p>
         </li>
 
         <li>
-          <h3>Celltype Scores</h3>
-          <p>Brief description and link to celltype scores.</p>
+          <h3>Single Cell Data</h3>
+          <p> Average and percent expression of all available genes, for all identified clusters. Format: Two nClusters x nGenes matrices in a single Anndata file. Size: ?? GB
+            <br/>
+            <a href="">Download</a>
+          </p>
+        </li>
+        <li>
+          <h3>Cell Metadata</h3>
+          <p>A collection of metadata information for each celltype.</p>
+          <ul>
+            <li>
+              <h3>Celltype Scores</h3>
+              <p>A likelihood score, for each celltype, of being present at each bead location.
+              Format: nCelltype x nBeads matrix in Anndata format. Size: ?? GB
+              <br/>
+              <a href="">Download</a>
+              </p>
+            </li>
+            <li>
+              <h3>Dissectates</h3>
+              <p>A dissectate names, for each celltype, where celltype score is greater than threshold (0.3).
+                Format: A zipped folder of CSV files. Size: ?? GB
+                <br/>
+                <a href="">Download</a>
+              </p>
+            </li>
+            <li>
+              <h3>Cell class</h3>
+              <p>A broader, assigned cell class for each celltype.
+                Format: A zipped folder of CSV files. Size: ?? GB
+                <br/>
+                <a href="">Download</a>
+              </p>
+            </li>
+          </ul>
         </li>
       </ul>
     </div>
@@ -45,8 +91,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Downloads | ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Downloads`}
+      description="Downloads">
       <HomepageHeader />
       <main>
         {/* <HomepageFeatures /> */}
