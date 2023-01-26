35319e6e9deff514d4ae43a3cc3501c76c1b022c
Mukund Raj <mraj@broadinstitute.org>
2023-01-26 10:41:39 -0500

    Update downloads page

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -33,7 +33,7 @@ function HomepageHeader() {
           <h3>Slide-seq Bead Positions</h3>
           <p>Slide-seq bead positions in the space of nissl images. Format: CSV files in zipped folder. Size: ?? GB.
           <br/>
-          <a href="">Download</a>
+          <a href="https://drive.google.com/drive/folders/1yfUFVvC-fPASfW5sv_UemgVY1Jkwe8ha?usp=sharing">Download</a>
           </p>
         </li>
 
@@ -41,47 +41,58 @@ function HomepageHeader() {
           <h3>Gene Expression</h3>
           <p>Number of UMI for each gene for all Slide-seq beads. Format: nBeads x nGenes matrix in Anndata format. Size: ?? GB
           <br/>
-          <a href="">Download</a>
+          <a href="https://drive.google.com/drive/folders/1fiJDY2HYOasiKOdKSIfk2ol2JTV_wTAl?usp=sharing">Download</a>
           </p>
         </li>
 
         <li>
-          <h3>Single Cell Data</h3>
-          <p> Average and percent expression of all available genes, for all identified clusters. Format: Two nClusters x nGenes matrices in a single Anndata file. Size: ?? GB
+          <h3>Single Cell Gene Expression and Metadata</h3>
+
+          <p> Average and percent expression of all available genes, for all
+            identified cells, along with metadata for each cell. The metadata
+            includes: a score indicating liklihood of cell being present at
+            each bead, dissectate(s) of origin (determined from location of
+            beads with score>threshold=0.3), and an assigned cell class.
+            Format: a set of files in <a
+              href="https://anndata.readthedocs.io/en/latest/">Anndata </a>
+            format, one file per puck/slice. A readme file with supplementary
+            information that common to all Anndata files in the folder is
+            included in the same folder. Size: ??-?? GB
+
             <br/>
-            <a href="">Download</a>
+            <a href="https://drive.google.com/drive/folders/1H3VRv-oDp9uGYt2w_fDNNPYUGWNCqEb7?usp=share_link">Download</a>
           </p>
         </li>
-        <li>
-          <h3>Cell Metadata</h3>
-          <p>A collection of metadata information for each celltype.</p>
-          <ul>
-            <li>
-              <h3>Celltype Scores</h3>
-              <p>A likelihood score, for each celltype, of being present at each bead location.
-              Format: nCelltype x nBeads matrix in Anndata format. Size: ?? GB
-              <br/>
-              <a href="">Download</a>
-              </p>
-            </li>
-            <li>
-              <h3>Dissectates</h3>
-              <p>A dissectate names, for each celltype, where celltype score is greater than threshold (0.3).
-                Format: A zipped folder of CSV files. Size: ?? GB
-                <br/>
-                <a href="">Download</a>
-              </p>
-            </li>
-            <li>
-              <h3>Cell class</h3>
-              <p>A broader, assigned cell class for each celltype.
-                Format: A zipped folder of CSV files. Size: ?? GB
-                <br/>
-                <a href="">Download</a>
-              </p>
-            </li>
-          </ul>
-        </li>
+        {/* <li> */}
+        {/*   <h3>Cell Metadata</h3> */}
+        {/*   <p>A collection of metadata information for each celltype.</p> */}
+        {/*   <ul> */}
+        {/*     <li> */}
+        {/*       <h3>Celltype Scores</h3> */}
+        {/*       <p>A likelihood score, for each celltype, of being present at each bead location. */}
+        {/*       Format: nCelltype x nBeads matrix in Anndata format. Size: ?? GB */}
+        {/*       <br/> */}
+        {/*       <a href="">Download</a> */}
+        {/*       </p> */}
+        {/*     </li> */}
+        {/*     <li> */}
+        {/*       <h3>Dissectates</h3> */}
+        {/*       <p>A dissectate names, for each celltype, where celltype score is greater than threshold (0.3). */}
+        {/*         Format: A zipped folder of CSV files. Size: ?? GB */}
+        {/*         <br/> */}
+        {/*         <a href="">Download</a> */}
+        {/*       </p> */}
+        {/*     </li> */}
+        {/*     <li> */}
+        {/*       <h3>Cell class</h3> */}
+        {/*       <p>A broader, assigned cell class for each celltype. */}
+        {/*         Format: A zipped folder of CSV files. Size: ?? GB */}
+        {/*         <br/> */}
+        {/*         <a href="">Download</a> */}
+        {/*       </p> */}
+        {/*     </li> */}
+        {/*   </ul> */}
+        {/* </li> */}
       </ul>
     </div>
   );
